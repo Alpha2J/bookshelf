dfe22133a256e943b6f563f1ce3d0bcbc622ec92
Jeb.Wang <3287566354@qq.com>
2018-11-30 17:58:18 +0800

    update: update doc

---
+++ src/cn/alpha2j/Main.java
@@ -0,0 +1,15 @@
+package cn.alpha2j;
+
+/**
+ * User: Jeb.Wang
+ * Date: 2018/11/30
+ * Time: 14:49
+ * GitHub: https://github.com/Alpha2J
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+//        Socket socket = n
+    }
+}
